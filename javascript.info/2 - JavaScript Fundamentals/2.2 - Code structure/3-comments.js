12b0f779f3a8b28524487842c2c8b6c2fccb4608
Treloso <diogo.trelosocnt@gmail.com>
2025-12-29 17:28:26 -0300

    Pequenas alterações nos documentos

--- javascript.info/2 - JavaScript Fundamentals/2.2 - Code structure/3-comments.js
+++ javascript.info/2 - JavaScript Fundamentals/2.2 - Code structure/3-comments.js
@@ -2,8 +2,8 @@
     //Ao passar do tempo, os códigos vão ficando cada vez mais complexos. Logo, é sempre útil adicionar comentários específicando algo que demandou maior trabalho.
     //Os comentário não afetam o código executado.
 
-    //Comentários de uma linha só começar com "//".
-    //Comentários Multilinha funcionam com "/*" no início e "*/" no final.
+    //Comentários de uma linha só começar com "//". (Atalho: Ctrl + / - Visual Studio Code)
+    //Comentários Multilinha funcionam com "/*" no início e "*/" no final. (Atalho: Shift + Alt + A - Visual Studio Code )
     //Às vezes pode ser últil para desabilitar uma parte do código por um tempo:
     /*
     alert('Olá'); 
