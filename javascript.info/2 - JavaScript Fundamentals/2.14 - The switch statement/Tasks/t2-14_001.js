8b8030489126e64350f557f00c6a2ea6afb3359a
Treloso <diogo.trelosocnt@gmail.com>
2026-01-19 13:54:33 -0300

    Tarefas "The switch statement" concluídas

--- javascript.info/2 - JavaScript Fundamentals/2.14 - The switch statement/Tasks/t2-14_001.js
+++ javascript.info/2 - JavaScript Fundamentals/2.14 - The switch statement/Tasks/t2-14_001.js
@@ -1,29 +1,28 @@
 'use strict';
-//Write the code using if..else which would correspond to the following switch:
-let browser = 'Edge'
-/*
+/* Write the code using if..else which would correspond to the following switch:
+
 switch (browser) {
   case 'Edge':
-    console.log( "You've got the Edge!" );
+    alert( "You've got the Edge!" );
     break;
 
   case 'Chrome':
   case 'Firefox':
   case 'Safari':
   case 'Opera':
-    console.log( 'Okay we support these browsers too' );
+    alert( 'Okay we support these browsers too' );
     break;
 
   default:
-    console.log( 'We hope that this page looks ok!' );
-}
-*/
+    alert( 'We hope that this page looks ok!' );
+} */
 
-if(browser == 'Edge'){
-  console.log( "You've got the Edge!" );
-} else if(browser == 'Chrome' || browser == 'Firefox' || browser == 'Safari' || browser == 'Opera'){
-  console.log( 'Okay we support these browsers too' );
+let browser = 'Chrome';
+
+if (browser === 'Edge') {
+    console.log("You've got the Edge!");
+} else if (browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera') {
+    console.log('Okay we support these browsers too');
 } else {
-  console.log( 'We hope that this page looks ok!' );
+    console.log('We hope that this page looks ok!');
 }
-
--- javascript.info/2 - JavaScript Fundamentals/2.14 - The switch statement/Tasks/t2-14_002.js
+++ javascript.info/2 - JavaScript Fundamentals/2.14 - The switch statement/Tasks/t2-14_002.js
@@ -1,25 +1,32 @@
 'use strict';
-//Rewrite the code below using a single switch statement:
-let a = 2;
+/* Rewrite the code below using a single switch statement:
 
-/*
+let a = +prompt('a?', '');
+
 if (a == 0) {
-    console.log( 0 );
+  alert( 0 );
 }
 if (a == 1) {
-    console.log( 1 );
+  alert( 1 );
 }
 
 if (a == 2 || a == 3) {
-    console.log( '2,3' );
-}
-*/
+  alert( '2,3' );
+} */
 
-switch(a) {
+let a = 1;
+
+switch (a) {
     case 0:
         console.log(0);
+        break;
+    
     case 1:
-        console.log(1)
-    case 2 || 3:
-        console.log('2, 3');
+        console.log(1);
+        break;
+
+    case 2:
+    case 3:
+        console.log('2,3');
+        break;
 }
