cf5becf405c63a2bd816239caffca5f52ff1fdcc
Treloso <diogo.trelosocnt@gmail.com>
2026-02-11 21:19:33 -0300

    Pequena despedida dos objetos

---
+++ javascript.info/4 - Objects the basics/4.05 - Constructor, operator new/5_objects_WeWillBeBack.js
@@ -0,0 +1,5 @@
+'use strict';
+//Objetos, nós voltaremos!
+    //Nesse capítulos nós apenas abordamos o básico sobre objetos e construtores. Eles são essenciais para aprender mais sobre tipos de dados e funções nos próximos capítulos.
+
+    //Depois de aprernder isso, nós retornamos para objetos e os cobrimos aprofundadamente nos capítulos "Prototypes, inheritance" e "Classes".
