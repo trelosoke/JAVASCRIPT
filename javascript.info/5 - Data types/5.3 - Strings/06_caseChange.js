2c743192c868c63fd7855301842817e909ab996c
trelosoke <diogo.trelosocnt@gmail.com>
2026-04-13 16:15:50 -0300

    feat(strings): implementa métodos de lowcase e downcase com strings

---
+++ javascript.info/5 - Data types/5.3 - Strings/06_caseChange.js
@@ -0,0 +1,8 @@
+'use strict';
+//Alterando a capitalização (caixa alta / caixa baixa)
+    //Os métodos '.toLowerCase()' e '.toUpperCase()' alteram a "caixa" do texto:
+        console.log('Interface'.toUpperCase()); //INTERFACE - caixa alta
+        console.log('Interface'.toLowerCase()); //interface - caixa baixa
+
+    //Ou se quisermos um único caractere de caixa baixa:
+        console.log('Interface'[0].toLowerCase()); //i
