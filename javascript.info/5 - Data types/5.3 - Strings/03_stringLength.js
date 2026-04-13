8dac6a9894e21e69d87f4383a35cfbd69bf61310
Treloso <diogo.trelosocnt@gmail.com>
2026-04-13 11:37:26 -0300

    feat(strings): adiciona apresentação da propriedade de string 'length'

---
+++ javascript.info/5 - Data types/5.3 - Strings/03_stringLength.js
@@ -0,0 +1,11 @@
+'use strict';
+//Tamanho da string
+    //A propriedade 'length' tem o tamanho da string:
+        console.log('Meu\n'.length); //4 - M, e, u, \n
+
+    //Note que \n é apenas um único caractere "especial", então o tamanho é realmente 4.
+
+                //'length' é uma propriedade
+                    //Pessoas que tiveram contato com algumas outras linguagens algumas vezes erram ao chamar 'str.length()' ao invés de só 'str.length'. Isso não funciona.
+
+                    //Por favor note que 'str.length' é uma propriedade numérica, não uma função/método. Não há necessidade de adicionar os parênteses após. Não '.length()', mas '.length'.
