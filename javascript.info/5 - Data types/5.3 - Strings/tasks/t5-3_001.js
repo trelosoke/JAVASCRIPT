8f533940b41fffb04f77659e4e41b102e3101b92
trelosoke <diogo.trelosocnt@gmail.com>
2026-04-14 15:23:48 -0300

    feat(strings): finaliza task 1, sobre capitalização em função

---
+++ javascript.info/5 - Data types/5.3 - Strings/tasks/t5-3_001.js
@@ -0,0 +1,11 @@
+/* 
+Write a function ucFirst(str) that returns the string str with the uppercased first character, for instance:
+*/
+
+function upperCaseFirst(string) {
+    if(!string) return string;
+
+    return string = string[0].toUpperCase() + string.slice(1);
+}
+
+console.log(upperCaseFirst("john") == "John");
