9d082667fa891b7577e7225cac50757677b06c5d
trelosoke <diogo.trelosocnt@gmail.com>
2026-04-15 15:22:58 -0300

    feat(arrays): adiciona lógica de arrays multidimensionais

---
+++ javascript.info/5 - Data types/5.4 - Arrays/09_multidimensionalArrays.js
@@ -0,0 +1,12 @@
+'use strict';
+//Arrays multidimensionais
+    //Arrays podem ter itens que também são arrays. Podemos usar isso para arrays multidimensionais, por exemplo para armazenar matrizes:
+        let matriz = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+        ];
+
+        console.log(matriz[0][0]); //1 - O primeiro valor do primeiro array interior
+        console.log(matriz[0][1]); //2
+        console.log(matriz[0][2]); //3
